f856ca5a84e2a5ef946a6004a02e6984ec7a17c5
João Lucas <62743404+joaoluke@users.noreply.github.com>
2024-01-11 23:35:33 -0300

    feat: add mega_sena script generate

---
+++ mega_sena.py
@@ -0,0 +1,8 @@
+import random
+
+def generate_sequences_mega_sena(quantity_of_sequences):
+    sequencies = []
+    for _ in range(quantity_of_sequences):
+        sequence = sorted(random.sample(range(1, 61), 6))
+        sequencies.append(sequence)
+    print(sequencies)
