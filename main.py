70d0b4fd7432371520d11d568cf3d1c247f2034a
João Lucas <62743404+joaoluke@users.noreply.github.com>
2024-01-11 23:35:59 -0300

    feat: add function checkLotteryGame

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
+from mega_sena import generate_sequences_mega_sena
+
+
+def checkLotteryGame(game, sequence_number):
+    if game == "Mega-Sena":
+        return generate_sequences_mega_sena(sequence_number)
+    elif game == "Lotofácil":
+        return "You can become a backend developer."
+    elif game == "Quina":
+        return "You can become a mobile app developer"
+    elif game == "Lotomania":
+        return "You can become a mobile app developer"
+    elif game == "Timemania":
+        return "You can become a mobile app developer"
+    elif game == "Dupla Sena":
+        return "You can become a mobile app developer"
+
 def main():
-    # Define the Brazilian lottery game options
     options = ['Mega-Sena', 'Lotofácil', 'Quina', 'Lotomania', 'Timemania', 'Dupla Sena']
     print("Choose a Brazilian lottery game:")
     for i, game in enumerate(options, start=1):
         print(f"{i}. {game}")
 
-    # Get the user's choice for the game
     game_choice = input("Enter the number of your choice for the lottery game: ")
 
-    # Check and respond to the game choice
     if game_choice.isdigit() and 1 <= int(game_choice) <= len(options):
         chosen_game = options[int(game_choice)-1]
         print(f"You chose: {chosen_game}")
@@ -16,12 +30,10 @@ def main():
         print("Invalid choice. Please enter a valid number for the lottery game.")
         return
 
-    # Ask for the number of sequences
     sequence_number = input("Enter the number of sequences you want (maximum 20): ")
 
-    # Check and respond to the sequence number choice
     if sequence_number.isdigit() and 1 <= int(sequence_number) <= 20:
-        print(f"You have chosen to generate {sequence_number} sequences for {chosen_game}.")
+        checkLotteryGame(chosen_game, int(sequence_number))
     else:
         print("Invalid choice. Please enter a valid number of sequences (1-20).")
 
